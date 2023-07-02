429ca640fb580c162dd1779a85dbd0fe3c6ebe75
Sergio <sergey.palomo@gmail.com>
2023-07-02 16:41:34 +0300

    3rd edit

--- 4-timer/script.js
+++ 4-timer/script.js
@@ -1,17 +1,13 @@
 'use strict';
 
 const timerBody = document.querySelector('.timer__body')
-const timerBody2 = document.querySelector('.timer__body-2')
 const pluralRules = new Intl.PluralRules('ru');
-const MILLISECONDS_IN_SECOND = 1000;
-const SECONDS_IN_MONTH = 60 * 60 * 24 * 30;
-const SECONDS_IN_DAY = 60 * 60 * 24;
-const SECONDS_IN_HOUR = 60 * 60;
+
 const SECONDS_IN_MINUTE = 60;
 const MOUTH_IN_YEAR = 12;
-const DAY_IN_MONTH = 30;
 const HOURS_IN_DAY = 24;
 const MINUTES_IN_HOUR = 60;
+
 const newYear = new Date(`${new Date().getFullYear() + 1}-01-01`);
 const PLURAL = {
   month: ['месяцев', 'месяца', 'месяц'],
@@ -21,31 +17,17 @@ const PLURAL = {
   second: ['секунд', 'секунды', 'секунда'],
 }
 
-function getDatesDiff_1() {
-  const datesDiffInSeconds = (newYear - new Date()) / MILLISECONDS_IN_SECOND
-  return {
-    month: Math.floor(datesDiffInSeconds / SECONDS_IN_MONTH),
-    day: Math.floor(datesDiffInSeconds / SECONDS_IN_DAY) % DAY_IN_MONTH,
-    hour: Math.floor(datesDiffInSeconds / SECONDS_IN_HOUR) % HOURS_IN_DAY,
-    minute: Math.floor(datesDiffInSeconds / SECONDS_IN_MINUTE) % MINUTES_IN_HOUR,
-    second: Math.floor(datesDiffInSeconds) % SECONDS_IN_MINUTE,
-  }
-}
 
-//На мой взгляд этот вариант более точный так как учитывает количество дней в месяце,
-// не зависит от округления и читается легко
-// Или возможно я не понял идею с преобразованием из разницы
-//или можно по разнице получать только часы минуты секунды...
-//я если честно не совсем понимаю почему этот вариант избыточен
 
-function getDatesDiff_2() {
-  const nextMonth = new Date(Number(new Date().getFullYear()), new Date().getMonth() + 1, 1 )
+function getDatesDiff() {
+  const dateNow = new Date()
+  const currentMonthDaysAmount = new Date(dateNow.getFullYear(), dateNow.getMonth() + 1, 0 ).getDate();
   return {
-    month: MOUTH_IN_YEAR - (new Date().getMonth() + 1),
-    day: Math.floor((nextMonth - new Date()) / (SECONDS_IN_DAY * MILLISECONDS_IN_SECOND)),
-    hour: HOURS_IN_DAY - new Date().getHours(),
-    minute: MINUTES_IN_HOUR - new Date().getMinutes(),
-    second: SECONDS_IN_MINUTE - new Date().getSeconds(),
+    month: MOUTH_IN_YEAR - (dateNow.getMonth() + 1),
+    day: currentMonthDaysAmount - (dateNow.getDate() + 1),
+    hour: HOURS_IN_DAY - dateNow.getHours(),
+    minute: MINUTES_IN_HOUR - dateNow.getMinutes(),
+    second: SECONDS_IN_MINUTE - dateNow.getSeconds(),
   }
 }
 
@@ -68,16 +50,8 @@ function pluralizeText(count, arr) {
   return result.join(' ')
 }
 
-function getTimerText_1() {
-  const diff = getDatesDiff_1()
-  const timerText = []
-  for ( const key in diff) {
-    timerText.push(pluralizeText(diff[key], PLURAL[key]))
-  }
-  return timerText.join(' ')
-}
-function getTimerText_2() {
-  const diff = getDatesDiff_2()
+function getTimerText() {
+  const diff = getDatesDiff()
   const timerText = []
   for (const key in diff) {
     timerText.push(pluralizeText(diff[key], PLURAL[key]))
@@ -86,8 +60,7 @@ function getTimerText_2() {
 }
 
 const runNewYearTimer = setInterval(() => {
-  timerBody.innerText = getTimerText_1()
-  timerBody2.innerText = getTimerText_2()
+  timerBody.innerText = getTimerText()
 }, 1000);
 
 if (newYear - Date.now() <= 0) {
